b53c1e82fa65bf803bb638e3067940216ca42828
RaimeiRaikiri <mikescutts10@gmail.com>
2025-05-16 00:55:48 +0100

    made changed to allow the app to start successfully

--- backend/__init__.py
+++ backend/__init__.py
@@ -4,31 +4,35 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
-from .models import User
 
 load_dotenv()
 db = SQLAlchemy()
-DB_NAME = "database"
+DB_NAME = "database.db"
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="../frontend/templates")
     CORS(app)
     
     app.config["SECRET_KEY"] = os.getenv("secret_key")
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
     
-    db.init_app()
+    db.init_app(app)
     
     from .views import views
     from .auth import auth
     
-    app.register_blueprint(views, "/views")
-    app.register_blueprint(auth, "/auth")
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
     
+    from .models import User, Task
+    
+    if not os.path.exists( "/backend" + DB_NAME):
+        with app.app_context():
+            db.create_all()
 
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
-    login_manager.init_app()
+    login_manager.init_app(app)
     
     @login_manager.user_loader
     def load_user(id):
