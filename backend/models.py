e2544eb3208cdc3b20b6d9903f3abe7cd662a2d0
RaimeiRaikiri <mikescutts10@gmail.com>
2025-05-15 22:29:07 +0100

    defined .gitignore and models for the backend database

---
+++ backend/models.py
@@ -0,0 +1,18 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    first_name = db.Column(db.String(120), nullable=False, unique=False)
+    password = db.Column(db.String(120), nullable=False, unique=False)
+    tasks = db.relationship("Task")
+    
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task_content = db.Column(db.String(5000), nullable=False, unique=False)
+    create_date = db.Column(db.DateTime(timezone=True), default=func.now())
+    deadline = db.Column(db.DateTime(timezone=True), nullable=True)
+    complete_status = db.Column(db.Boolean, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
